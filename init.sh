cd15f1cac9166e321d588408b25ca84e6b44fdc7
kondo takeshi <chaspy@users.noreply.github.com>
2020-11-18 23:06:26 +0900

    Install moloch

--- init.sh
+++ init.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+set -eu
 apt-get update -y
+apt -y --fix-broken install
+apt-get install -y libwww-perl \
+                   libjson-perl \
+                   libyaml-dev
+curl -s https://s3.amazonaws.com/files.molo.ch/builds/ubuntu-20.04/moloch_2.4.2-1_amd64.deb -o moloch.deb
+sudo dpkg -i moloch.deb
